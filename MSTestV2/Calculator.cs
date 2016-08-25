6bb13ae368570634ab60430939768dd499cb04bf
Ian Ceicys <ian.ceicys@autodesk.com>
2016-08-25 09:07:41 -0400

    Adding Solution File and Project for Repo Steps
    Solution File and Repo

---
+++ MSTestV2/Calculator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MSTestV2
+{
+    public static class Calculator
+    {
+        internal static int Add(int a, int b)
+        {
+            int c = a + b;
+            return c;
+        }
+    }
+}
---
+++ MSTestV2/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MSTestV2
+{
+    [TestClass]
+    public class Class1
+    {
+        [DataTestMethod]
+        [DataRow(1, 1, 2)]
+        [DataRow(1, 3, 4)]
+        [DataRow(2, 4, 6)]
+        [DataRow(6, 1, 7)]
+        public void AddTest(int a, int b, int result)
+        {
+            Assert.AreEqual(result, Calculator.Add(a, b));
+        }
+    }
+}
